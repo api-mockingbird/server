f7cb951723cbe99d0ba7d2607f0bba9d71ed5ccf
Jaehoo Im <im.jaehoo1@gmail.com>
2021-09-23 03:46:22 +0900

    inject req res into context

--- src/context.ts
+++ src/context.ts
@@ -1,9 +1,24 @@
 import { PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
 
 export interface Context {
   prisma: PrismaClient;
+  req: Request;
+  res: Response;
 }
 
 const prisma = new PrismaClient();
 
-export const context: Context = { prisma };
+export const context = ({
+  req,
+  res,
+}: {
+  req: Request;
+  res: Response;
+}): Context => {
+  return {
+    prisma,
+    req,
+    res,
+  };
+};
