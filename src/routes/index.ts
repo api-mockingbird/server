3945c58aa18600709c12dccca75642afcfd604c9
Jaehoo Im <im.jaehoo1@gmail.com>
2021-11-03 03:14:42 +0900

    config limiter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -32,68 +32,77 @@ const limiter = rateLimit({
   },
 });
 
-router.use(limiter);
+router.all(
+  '/*',
+  limiter,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { params, subdomains, method } = req;
+    const urlPath = `/${params['0']}`;
+    const subdomain = subdomains[0];
+    const availableMethods = [
+      'GET',
+      'POST',
+      'PUT',
+      'PATCH',
+      'DELETE',
+      'OPTIONS',
+    ];
 
-router.all('/*', async (req: Request, res: Response, next: NextFunction) => {
-  const { params, subdomains, method } = req;
-  const urlPath = `/${params['0']}`;
-  const subdomain = subdomains[0];
-  const availableMethods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'];
+    if (subdomains.length > 1) {
+      return next(new BadRequest());
+    }
 
-  if (subdomains.length > 1) {
-    return next(new BadRequest());
-  }
+    if (
+      subdomain === 'www' ||
+      subdomain === 'app' ||
+      (process.env.NODE_ENV !== 'production' && urlPath === '/graphql')
+    ) {
+      return next();
+    }
 
-  if (
-    subdomain === 'www' ||
-    subdomain === 'app' ||
-    (process.env.NODE_ENV !== 'production' && urlPath === '/graphql')
-  ) {
-    return next();
-  }
+    if (!availableMethods.includes(method)) {
+      return next(new MethodNotAllowed());
+    }
 
-  if (!availableMethods.includes(method)) {
-    return next(new MethodNotAllowed());
-  }
+    const mockEndpoint = await getMockEndpointByRequestInfo(db, {
+      userId: subdomain,
+      httpMethod: method as HttpMethod,
+      urlPath,
+    });
 
-  const mockEndpoint = await getMockEndpointByRequestInfo(db, {
-    userId: subdomain,
-    httpMethod: method as HttpMethod,
-    urlPath,
-  });
+    if (!mockEndpoint) {
+      return next(new NotFound());
+    }
 
-  if (!mockEndpoint) {
-    return next(new NotFound());
-  }
+    const user = await getUserById(db, mockEndpoint.userId);
 
-  const user = await getUserById(db, mockEndpoint.userId);
+    if (
+      user!.isTemp &&
+      new Date(user!.createdAt) < new Date(Date.now() - ONE_HOUR)
+    ) {
+      return next(new NotFound());
+    }
 
-  if (
-    user!.isTemp &&
-    new Date(user!.createdAt) < new Date(Date.now() - ONE_HOUR)
-  ) {
-    return next(new NotFound());
-  }
+    res.status(mockEndpoint.httpStatus);
 
-  res.status(mockEndpoint.httpStatus);
+    if (mockEndpoint.httpHeaders) {
+      res.set(JSON.parse(mockEndpoint.httpHeaders));
+    }
 
-  if (mockEndpoint.httpHeaders) {
-    res.set(JSON.parse(mockEndpoint.httpHeaders));
-  }
+    res.type(mockEndpoint.responseContentType);
 
-  res.type(mockEndpoint.responseContentType);
+    if (mockEndpoint.timeout) {
+      await sleep(mockEndpoint.timeout);
+    }
 
-  if (mockEndpoint.timeout) {
-    await sleep(mockEndpoint.timeout);
-  }
+    try {
+      const body = JSON.parse(mockEndpoint.httpResponseBody);
 
-  try {
-    const body = JSON.parse(mockEndpoint.httpResponseBody);
-
-    res.send(body);
-  } catch {
-    res.send(mockEndpoint.httpResponseBody);
-  }
-});
+      res.send(body);
+    } catch {
+      res.send(mockEndpoint.httpResponseBody);
+    }
+  },
+);
 
 export default router;
